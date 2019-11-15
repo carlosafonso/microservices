23ba4fc633980963f30741392f70b7a585b18538
Carlos Afonso Perez <cafonsop@amazon.com>
2019-11-15 13:49:10 +0100

    Introduce random erros on demand

--- src/word/main.py
+++ src/word/main.py
@@ -1,3 +1,4 @@
+import os
 import random
 from flask import Flask
 app = Flask(__name__)
@@ -5,6 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    random_error_probability = 0
+    if 'RANDOM_ERROR_PROBABILITY' in os.environ:
+        random_error_probability = float(os.environ['RANDOM_ERROR_PROBABILITY'])
+    print("Random error prob: {}".format(random_error_probability))
+    if random.randint(0, 1) < random_error_probability:
+        raise Exception("Random exception occured!")
+
     words = ['function', 'overcharge', 'consensus', 'pest', 'related', 'locate', 'earwax', 'refund', 'lead', 'stage']
     word = random.choice(words)
     print("Selected word is {}".format(word))
