dfc713b753c19d46a4d8d708611af5de5239d5f4
Carlos Afonso <elfonsi@gmail.com>
2022-11-11 14:49:40 +0000

    Bump Terraform Google providers to version 4.43

--- gcp/main.tf
+++ gcp/main.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.37.0"
+      version = "~> 4.43.0"
+    }
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "~> 4.43.0"
     }
   }
 }
