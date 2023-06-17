1e5b10c4a8dcf034d0a1b32cf10d9b011b76f8ce
Carlos Afonso <fonsi@google.com>
2023-06-17 16:26:02 +0000

    Use the Slim framework in frontend service.
    This will come handy later on when we add tracing, as Slim is one of the frameworks
    with automatic integration with OpenTelemetry.

--- src/frontend/index.php
+++ src/frontend/index.php
@@ -7,93 +7,105 @@ use Google\Cloud\PubSub\PubSubClient;
 use League\Plates\Engine;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-$log = new Logger('frontend');
-$log->pushHandler(new StreamHandler('php://stdout'));
+$app = AppFactory::create();
 
-$templates = new Engine(__DIR__ . '/templates');
+$app->get('/', function (Request $request, Response $response, $args) {
+    $log = new Logger('frontend');
+    $log->pushHandler(new StreamHandler('php://stdout'));
 
-$env = getenv('MICROSERVICES_ENV');
-if (empty($env)) {
-    $env = 'dev';
-}
+    $templates = new Engine(__DIR__ . '/templates');
 
-$log->info("Environment is $env");
+    $env = getenv('MICROSERVICES_ENV');
+    if (empty($env)) {
+        $env = 'dev';
+    }
 
-[$podName, $podIp] = [getenv('MSVC_POD_NAME'), getenv('MSVC_POD_IP')];
-$log->info("Retrieved pod name and IP", ['pod_name' => $podName, 'pod_ip' => $podIp]);
+    $log->info("Environment is $env");
 
-$fontColorSvcEndpoint = getenv('FONT_COLOR_SVC');
-$fontSizeSvcEndpoint = getenv('FONT_SIZE_SVC');
-$wordSvcEndpoint = getenv('WORD_SVC');
+    [$podName, $podIp] = [getenv('MSVC_POD_NAME'), getenv('MSVC_POD_IP')];
+    $log->info("Retrieved pod name and IP", ['pod_name' => $podName, 'pod_ip' => $podIp]);
 
-$log->info(
-    "Service endpoints retrieved",
-    [
-        'font-color' => $fontColorSvcEndpoint,
-        'font-size' => $fontSizeSvcEndpoint,
-        'word' => $wordSvcEndpoint,
-    ]
-);
+    $fontColorSvcEndpoint = getenv('FONT_COLOR_SVC');
+    $fontSizeSvcEndpoint = getenv('FONT_SIZE_SVC');
+    $wordSvcEndpoint = getenv('WORD_SVC');
 
-$pubSubEventsTopic = getenv('PUBSUB_EVENTS_TOPIC');
-$emitToPubSub = $pubSubEventsTopic !== false && !empty($pubSubEventsTopic);
-
-$httpClient = new HttpClient(timeout: 1);
-
-$word = $color = $size = null;
-
-try {
-    $wordResponse = $httpClient->get($wordSvcEndpoint);
-    $word = json_decode($wordResponse)->word;
-} catch (\Exception $e) {
-    $log->critical('Failed to get word', ['exception' => $e]);
-    $word = '(ERROR)';
-}
-
-try {
-    $colorResponse = $httpClient->get($fontColorSvcEndpoint);
-    $color = json_decode($colorResponse)->color;
-} catch (\Exception $e) {
-    $log->critical('Failed to get font color', ['exception' => $e]);
-    $color = 'red';
-}
-
-try {
-    $sizeResponse = $httpClient->get($fontSizeSvcEndpoint);
-    $size = json_decode($sizeResponse)->size;
-} catch (\Exception $e) {
-    $log->critical('Failed to get font size', ['exception' => $e]);
-    $size = 50;
-}
-
-$log->info("Invoked all services", ['font-color' => $color, 'font-size' => $size, 'word' => $word]);
-
-if ($emitToPubSub) {
-    $pubSub = new PubSubClient();
-    $topic = $pubSub->topic($pubSubEventsTopic);
-
-    // Publish an event into the Pub/Sub events topic.
-    $eventPayload = [
-        'word' => $word,
-        'color' => $color,
-        'size' => $size,
-    ];
-    $topic->publish(['data' => json_encode($eventPayload)]);
-
-    $log->info("Sent event to Pub/Sub");
-}
-
-echo(
-    $templates->render(
-        'index',
+    $log->info(
+        "Service endpoints retrieved",
         [
+            'font-color' => $fontColorSvcEndpoint,
+            'font-size' => $fontSizeSvcEndpoint,
+            'word' => $wordSvcEndpoint,
+        ]
+    );
+
+    $pubSubEventsTopic = getenv('PUBSUB_EVENTS_TOPIC');
+    $emitToPubSub = $pubSubEventsTopic !== false && !empty($pubSubEventsTopic);
+
+    $httpClient = new HttpClient(timeout: 1);
+
+    $word = $color = $size = null;
+
+    try {
+        $wordResponse = $httpClient->get($wordSvcEndpoint);
+        $word = json_decode($wordResponse)->word;
+    } catch (\Exception $e) {
+        $log->critical('Failed to get word', ['exception' => $e]);
+        $word = '(ERROR)';
+    }
+
+    try {
+        $colorResponse = $httpClient->get($fontColorSvcEndpoint);
+        $color = json_decode($colorResponse)->color;
+    } catch (\Exception $e) {
+        $log->critical('Failed to get font color', ['exception' => $e]);
+        $color = 'red';
+    }
+
+    try {
+        $sizeResponse = $httpClient->get($fontSizeSvcEndpoint);
+        $size = json_decode($sizeResponse)->size;
+    } catch (\Exception $e) {
+        $log->critical('Failed to get font size', ['exception' => $e]);
+        $size = 50;
+    }
+
+    $log->info("Invoked all services", ['font-color' => $color, 'font-size' => $size, 'word' => $word]);
+
+    if ($emitToPubSub) {
+        $pubSub = new PubSubClient();
+        $topic = $pubSub->topic($pubSubEventsTopic);
+
+        // Publish an event into the Pub/Sub events topic.
+        $eventPayload = [
+            'word' => $word,
             'color' => $color,
             'size' => $size,
-            'word' => $word,
-            'env' => $env,
-            'podName' => $podName,
-            'podIp' => $podIp,
-        ]
-    )
-);
+        ];
+        $topic->publish(['data' => json_encode($eventPayload)]);
+
+        $log->info("Sent event to Pub/Sub");
+    }
+
+    $response
+        ->getBody()
+        ->write(
+            $templates->render(
+                'index',
+                [
+                    'color' => $color,
+                    'size' => $size,
+                    'word' => $word,
+                    'env' => $env,
+                    'podName' => $podName,
+                    'podIp' => $podIp,
+                ]
+            )
+        );
+    return $response;
+});
+
+$app->run();
